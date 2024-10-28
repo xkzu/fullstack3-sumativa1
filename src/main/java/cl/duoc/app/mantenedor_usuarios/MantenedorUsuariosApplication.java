37a129af92140bebed75fa1c7d506052076e6fc2
Kzu <alvaro.vega.pavez@gmail.com>
2024-10-28 18:43:54 -0300

    estructura inicial

---
+++ src/main/java/cl/duoc/app/mantenedor_usuarios/MantenedorUsuariosApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.app.mantenedor_usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MantenedorUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MantenedorUsuariosApplication.class, args);
+	}
+
+}
